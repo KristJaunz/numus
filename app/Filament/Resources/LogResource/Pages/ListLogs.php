f567f735e2fbf86152ba3f09648a2aa37a037e58
Kristians Jaunzems <kristians@initium.lv>
2025-03-06 14:56:14 +0200

    Update ListLogs.php

--- app/Filament/Resources/LogResource/Pages/ListLogs.php
+++ app/Filament/Resources/LogResource/Pages/ListLogs.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\LogResource\Pages;
 
 use App\Filament\Resources\LogResource;
+use App\Models\Tender;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListLogs extends ListRecords
@@ -15,5 +17,27 @@ class ListLogs extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [
+            'All' => Tab::make('Visi')
+                ->modifyQueryUsing(fn($query) => $query),
+        ];
+
+        $tabs['Darījumi'] = Tab::make('Darījumi')
+            ->modifyQueryUsing(fn($query) => $query->whereNotNull('store_doc_id'));
+
+        $tabs['Apstiprināšana'] = Tab::make('Apstiprināšana')
+            ->modifyQueryUsing(fn($query) => $query->whereNull('store_doc_id'));
+
+        return $tabs;
+
+    }
+
+    public function getDefaultActiveTab(): string|int|null
+    {
+        return 'All';
     }
 }
